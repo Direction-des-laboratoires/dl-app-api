72ef3ab19c76490c2768e3aff1c2cd54f7b3f4d9
dave-laye <pt90126@gmail.com>
2026-03-15 19:55:11 +0000

    github action fix 1.2

--- src/api/labs/schemas/lab.schema.ts
+++ src/api/labs/schemas/lab.schema.ts
@@ -1,5 +1,5 @@
 import * as mongoose from 'mongoose';
-import * as autopopulate from 'mongoose-autopopulate';
+import autopopulate from 'mongoose-autopopulate';
 
 export const LabSchema = new mongoose.Schema({
   name: {
